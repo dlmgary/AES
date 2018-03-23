1b052dadc2310af699b6e488a3113bc99c04e769
dlmgary <Gary@GMacBook.local>
2018-03-22 22:29:56 -0400

    body

--- strart_implemenattion.py
+++ strart_implemenattion.py
@@ -2,6 +2,7 @@
 
 import aes_constants 
 import logging
+import sys
 
 s_box = aes_constants.S_BOX
 s_box_inv = aes_constants.S_BOX_INV
@@ -52,7 +53,7 @@ def hex_to_bin_string_align(hex_input, sizeof_in_bits):
 ## 
 #
 #
-##
+## 
 def get_blocks(input_hex, Nk, block_size_bits):
    
    array_bytes = []
@@ -105,6 +106,7 @@ def RotWord(word_list):
    new_list.append(word_list[0])
    return new_list
    
+   
 ## 
 # Uses the values of word to pick corresponsing values from S-Box and 
 # returs those values
@@ -123,6 +125,27 @@ def sub_word(word):
    
    new_word = array_to_int(new_list, 8)
    return new_word
+   
+   
+## 
+# Uses the values of word to pick corresponsing values from S-Box-Inv and 
+# returs those values
+##
+def inv_sub_word(word):
+   new_list = []
+   new_word = 0
+   byte_list = get_bytes(word, 32)
+   row = 0
+   index = 0
+   
+   for i, item in enumerate(byte_list):
+      row = int(hex_to_str_align(item, 8)[2],16)
+      index = int(hex_to_str_align(item, 8)[3],16)
+      new_list.append(s_box_inv[row][index])
+   
+   new_word = array_to_int(new_list, 8)
+   return new_word
+
 
 ##
 # Generates a key schedule containing Nb (Nr + 1) keyes
@@ -169,7 +192,7 @@ def key_expansion(key, Nk):
 
 #   for i, item in enumerate(w):
 #      print "{} {}".format(i, hex(item))
-
+   logging.info("Key expansion {}".format(w))
    return w
    
 
@@ -228,17 +251,13 @@ def modular_division(int1):
    poly_degree = len(spoly)-1
    
    while(True):
-      if poly_degree >= b1_degree:
+      if poly_degree > b1_degree: 
          break
 
       quotient = b1_degree - poly_degree 
       intermediate = int(irr_poly,2) << int(quotient)
-             
-#   if type(sb1) is str:
       reminder = intermediate ^ int(sb1,2)
-#      else:
-#         reminder = intermediate ^ sb1
-#
+
       logging.debug("sb1:\t\t{}\t{}".format(sb1, type(sb1)))
       logging.debug("intermediate:\t{}\t{}".format(bin(intermediate).lstrip("0b"), type(intermediate)))
       logging.debug("Reminder:\t{}\t{}".format(bin(reminder).lstrip("0b"), type(reminder)))
@@ -310,7 +329,7 @@ def modular_division_2(int1, modulus_bin):
 
 
 
-class State_Array():
+class State_Array:
    Nr = 0       # Number of rounds
    Nk = 0       # Key length
    Nb = 4       # Block size (always 4)
@@ -327,12 +346,8 @@ class State_Array():
       for row in range(0, 4):
          new = []
          for col in range(0, 4):
-            new.append(0)
+            new.append(int(0))
          self.matrix.append(new)
-#            self.matrix[row][col] = 0
-#      for col in range(0, self.Nrow):
-#         self.matrix.append([0]*self.Ncol)
-
    
    def get_state(self):
       return self.matrix
@@ -358,7 +373,6 @@ class State_Array():
    def add_round_key(self, r_key_array, col_no):
       for i in range (0, 4):
          old_value = self.matrix[i][col_no]
-#         print "{} ^ {}".format(hex(old_value), hex(r_key_array[i]))
          self.matrix[i][col_no] = old_value ^ r_key_array[i]
 
    def get_state_array(self):
@@ -369,33 +383,63 @@ class State_Array():
          row = self.get_row(i)
          new_row =  get_bytes(sub_word(array_to_int(row, 8)),32)
          self.set_row(i, new_row)
-         
+    
+   def inv_sub_bytes(self):
+      for i in range (0,4):
+         row = self.get_row(i)
+         new_row =  get_bytes(inv_sub_word(array_to_int(row, 8)),32)
+         self.set_row(i, new_row)
+
          
    def set_initial_state(self, input_array):
-#      s[r,c]=in[r+4c]
       for row in range (0, 4):
          for column in range(0,4):
-            self.matrix[row][column] = input_array[row+4*column]
+            self.matrix[row][column] = int(input_array[row+4*column])
    
    ##
    # RotWord() perform a cyclic permutation
    # Input  : list [a0,a1,a2,a3] 
    # Returns: list [a1,a2,a3,a0]
    ##
-   def RotWord(word_list):
+   def RotWord(self,word_list):
       new_list = []
       new_list = word_list[1:4]
       new_list.append(word_list[0])
       return new_list
    
-   def shift_columns(self):
+       
+   ##
+   # RotWord() perform a cyclic permutation
+   # Input  : list [a0,a1,a2,a3] 
+   # Returns: list [a3,a0,a1,a2]
+   ##
+   def InvRotWord(self, word_list):  
+      new_list = word_list[0:3]
+      new_list.insert(0, word_list[3])
+      new_list = [int(i) for i in new_list]
+      
+      logging.debug("[+] InvRotWord() input: = {}".format(word_list))
+      logging.debug("[+] output: = {} {}".format(new_list, type(new_list[0])))
+
+      return new_list 
+ 
+   
+   def shift_rows(self):
       for i in range (0,4):
          row = self.get_row(i)
          for _ in range (0, i):
-            row = RotWord(row) 
+            row = self.RotWord(row) 
             self.set_row(i, row)
+ 
+   def inv_shift_rows(self):
+      for i in range (0,4):
+         row = self.get_row(i)
+         for _ in range (0, i):
+            row = self.InvRotWord(row)
+            self.set_row(i, row)
+            
+            
 
-# 
 #   MixColumns() operates on a column-by-column manner. 
 # theats each columns a four-term polynomial. In this case each 
 # term represent an 8-bit value. The columns are considered as 
@@ -410,7 +454,8 @@ class State_Array():
 # S'[1,c] = S[0,c] + ({02}*S[1,c]) + ({03}*S[2,c]) + S[3,c]
 # S'[2,c] = S[0,c] + S[1,c] + ({02}*S[2,c]) + ({03}*S[3,c])
 # S'[3,c] = ({03}*S[0,c]) + S[1,c] + S[2,c] + ({02}*S[3,c])
-#  
+
+
    def mix_columns(self):
       for i in range (0, 4):
          new_column = []
@@ -422,84 +467,216 @@ class State_Array():
          self.set_col(i, new_column)
          logging.debug("mix_columns:\n\tNew column[{}] = {}".format(i, new_column))   
 
+   def xtimeb(self, x):
+      return xtime(xtime(xtime(x))) ^ xtime(x) ^ x
+      
+   def xtimed(self, x):
+      return xtime(xtime(xtime(x))) ^ xtime(xtime(x)) ^ x
+      
+   def xtime9(self, x):
+      return xtime(xtime(xtime(x))) ^ x
 
+   def xtimee(self, x):
+      return xtime(xtime(xtime(x))) ^ xtime(xtime(x)) ^ xtime(x) ^ x
+
+#   InvMixColumns() operates on a column-by-column manner. 
+# theats each columns a four-term polynomial. In this case each 
+# term represent an 8-bit value. The columns are considered as 
+# polynomials GF(2^8) and  multiplied module x^4 + 1 with a 
+# fixed polinomial a(x) given:
+#   
+# a(x) = {0b}x^3 + {0d}x^2 + {09}x + {0e}
+#   
+# S'(x) = a(x) x S(x)
+#
+# S'[0,c] = ({0e}*S[0,c]) + ({0b}*S[1,c]) + ({0d}*S[2,c]) + ({09}*S[3,c])
+# S'[1,c] = ({09}*S[0,c]) + ({0e}*S[1,c]) + ({0b}*S[2,c]) + ({0d}*S[3,c])
+# S'[2,c] = ({0d}*S[0,c]) + ({09}*S[1,c]) + ({0e}*S[2,c]) + ({0b}*S[3,c])
+# S'[3,c] = ({0b}*S[0,c]) + ({0d}*S[1,c]) + ({09}*S[2,c]) + ({0e}*S[3,c])
+#
+# 0b = 0b00001011   xtime(xtime(xtime(x))) ^ xtime(x) ^ x
+# 0d = 0b00001101   xtime(xtime(xtime(x))) ^ xtime(xtime(x)) ^ x
+# 09 = 0b00001001   xtime(xtime(xtime(x))) ^ x
+# 0e = 0b00001110   xtime(xtime(xtime(x))) ^ xtime(xtime(x)) ^ xtime(x) ^ x
 
       
-def encrypt_plain_text(plain_text, key_list):
+   def inv_mix_columns(self):
+      for i in range (0, 4):
+         new_column = []
+         c = self.get_col(i)
+         new_column.append(self.xtimee(c[0]) ^ self.xtimeb(c[1]) ^ self.xtimed(c[2]) ^ self.xtime9(c[3]))
+         new_column.append(self.xtime9(c[0]) ^ self.xtimee(c[1]) ^ self.xtimeb(c[2]) ^ self.xtimed(c[3]))
+         new_column.append(self.xtimed(c[0]) ^ self.xtime9(c[1]) ^ self.xtimee(c[2]) ^ self.xtimeb(c[3]))
+         new_column.append(self.xtimeb(c[0]) ^ self.xtimed(c[1]) ^ self.xtime9(c[2]) ^ self.xtimee(c[3]))
+         self.set_col(i, new_column)
+         logging.debug("mix_columns:\n\tNew column[{}] = {}".format(i, new_column))   
+   
+   def get_output_bytes(self): 
+      output_array = []
+      output = 0x0
+      
+      for i in range(0,4):
+         output_array = output_array + self.get_col(i) 
+      
+      for i in output_array:
+         output = (output << 8) | int(i)
+                  
+      return output
+
+      
+def encrypt(plain_text, key_list):
    plain_text = plain_text
    cypher_tex = ""
    key_list = key_list
     
    state = State_Array(128, "", 10)
-   
+   print "Start encrypt {}".format(hex(state.get_output_bytes()), type(state.get_output_bytes()))
+
    Nb = 4 
    Nr = 11
    
    state.set_initial_state(get_bytes(plain_text, 128))
-      
+   print "encrypt {} {}".format(0, hex(state.get_output_bytes()), type(state.get_output_bytes()))
+
    ## Pupulate state array with first 4 keys
    for i in range(0, Nb):
       new_col_vals = get_bytes(key_list[i], 32)
       state.add_round_key(new_col_vals, i)
-    
-   
+
    ## Does loops throguh the algorith Nr-1 times
    for round_no in range(0, (Nr -2)):
       state.sub_bytes()
-      state.shift_columns()
+      state.shift_rows()
       state.mix_columns()
       for i in range(0,4):
          key_no = (round_no+1)*4 + i
-         print "element no = {}".format(key_no)
          key = get_bytes(key_list[key_no], 32)
          state.add_round_key(key, i)
 
-
    state.sub_bytes()
-   state.shift_columns()
+   state.shift_rows()
+   
    for i in range(0,4):
       key_no = (Nr-1)*4 + i
-      print "element no = {}".format(key_no)
       key = get_bytes(key_list[key_no], 32)
       state.add_round_key(key, i)
 
-   
-   ## Prints array in hex
+   """
+   ## Prints array in hex 
    array =  state.get_state_array()
    for row in array:
       print ""
       for column in row:
          print hex(column).rstrip("L"),
    print ""
-   ## 
+   ##      
+   """
+   
+   cypher_tex = state.get_output_bytes()
+   
+   return cypher_tex
+
+ 
+def decrypt(cypher_text, key_list):
+   cypher_text = cypher_text
+   plain_txt = ""
+   key_list = key_list
+
+   state = State_Array(128, "", 10)
+   print "Start decrypt {}".format(hex(state.get_output_bytes()), type(state.get_output_bytes()))
+
+   Nb = 4 
+   Nr = 11
+   print ""
+
+
+   key_array = get_bytes(cypher_text, 128)
+   key_array = [int(i) for i in key_array]
+
+   state.set_initial_state(key_array)
+   
+#   print "Decrypt {} {}".format(0, hex(state.get_output_bytes()), type(state.get_output_bytes()))
+
+   
+   ## Pupulate state array with first 4 keys
+   offset = len(key_list)-4
+   for i in range(0, Nb):
+      new_col_vals = get_bytes(key_list[offset + i], 32)
+      state.add_round_key(new_col_vals, i)
+      print " key {} {}".format(new_col_vals, offset + i),
+
+   
+   
+   ## Does loops throguh the algorith Nr-1 times
+   for round_no in range((Nr -2), 0, -1):
+      print "Decrypt {} {} {}".format(10-round_no, hex(state.get_output_bytes()), type(state.get_output_bytes()))
+
+      ## Shifts collumns
+      state.inv_shift_rows() 
+      print "   shift_row {}".format(hex(state.get_output_bytes()))
+
+      ## Substitutes bytes
+      state.inv_sub_bytes()
+      print "   sub_bytes {}".format(hex(state.get_output_bytes()))
+
+      ## Adds round key
+      for i in range(0,4):
+         key_no = round_no*4 + i
+#         print "w[{}]".format(round_no*4+i)
+         key = get_bytes(key_list[key_no], 32)
+         state.add_round_key(key, i)
+#         print " key {} {}".format(key_no, key),
+         
+      print " \nadd_rnd_key {}".format(hex(state.get_output_bytes()))
+
+      
+      state.inv_shift_rows()
+
+   state.inv_shift_rows() 
+   state.inv_sub_bytes()
+   
+   for i in range(3, -1, -1):
+#      print "w[{}]".format(i)
+      key = get_bytes(key_list[key_no], 32)
+      state.add_round_key(key, i)
       
 
+#   ## Prints array in hex
+#   array =  state.get_state_array()
+#   for row in array:
+#      print ""
+#      for column in row:
+#         print hex(column).rstrip("L"),
+#   print ""
+#   ##      
+
+   plain_text = state.get_output_bytes()
+   return plain_text
+ 
+ 
    
 def main(): 
-#   logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(message)s')
+#   logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 
-
-   input_str = 0x3243f6a8885a308d313198a2e0370734
-   cypher_key_128 = 0x2b7e151628aed2a6abf7158809cf4f3c
+#   plain_text = 0x3243f6a8885a308d313198a2e0370734
+#   key = 0x2b7e151628aed2a6abf7158809cf4f3c
    
-   round_keys = key_expansion(cypher_key_128, 4)
+   plain_text = 0x00112233445566778899aabbccddeeff
+   key = 0x000102030405060708090a0b0c0d0e0f
    
-   encrypt_plain_text(input_str, round_keys)
- 
-
-
-#   new_state.set_row(2, [5, 5, 5, 5])
-#   new_state.set_col(3, [10,12,14,16,40,34,12,1])
-#   print new_state.get_state_array()
-#   print hex(round_keys[0])
-#   new_state.get_row(0)
-#   new_state.add_round_key(get_bytes(round_keys[0], 32), 2)
-#   print new_state.get_state_array()
-#   
-#   new_state.add_round_key(get_bytes(round_keys[1], 32), 2)
-#    
-#   print new_state.get_state_array() 
-
+   round_keys = key_expansion(key, 4)
+   round_keys = [int(i) for i in round_keys]
+   
+#   cypher_text = encrypt(plain_text, round_keys)
+    
+#   print ""
+#   print "Plain text:\t{}".format(hex(plain_text))
+#   print "Key       :\t{}".format(hex(key))
+#   print "Cypher text:\t{}".format(hex(cypher_text))
+#   print "Correct txt:\t{}".format(hex(0x69c4e0d86a7b0430d8cdb78070b4c55a))
+   
+   cypher_text = 0x69c4e0d86a7b0430d8cdb78070b4c55a
+   plain_text = decrypt(cypher_text, round_keys)
    
 if __name__ == "__main__":
    main()
