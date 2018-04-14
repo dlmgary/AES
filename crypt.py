95a8a8635746c4757ceb0a1b117ef09d31b42263
Gary <dlmgary@hotmail.com>
2018-04-14 11:31:00 -0400

    Update crypt.py
    Simplified AES interface

--- crypt.py
+++ crypt.py
@@ -1,39 +1,15 @@
 #!/usr/bin/python
 
-from aes import encrypt, key_expansion
+from aes import aes, key_expansion
 import argparse
-
-def read_block(fd, block_size):
-   while True:
-      block = fd.read(block_size)
-      if not block:
-         break
-         
-      else:
-         hex_block = ""
-         for ch in block:
-            hex_block += str(hex(ord(ch))).lstrip("0x").zfill(2)
-         yield hex_block
-
-
-def write_block(fd_in, content):
-   number_of_chars = len(content)/2
-   for ch in range(0, number_of_chars):
-      fd_in.write(chr(int(content[ch*2:ch*2+2],16)))
-
-def byte_array(input_hex, byte_no):
-   array_bytes = []
-   mask = 0xFF
-   
-   for i in range(0, byte_no):
-      array_bytes.append(input_hex >> i*8 & mask)
-
-   return array_bytes[::-1]
 
 
 def main():
-
-   parse = argparse.ArgumentParser(description="Custom AES implementation. By deault it will encrypt/decrypt text using 128-bit key size. ")
+   description = """
+   Custom AES implementation using python. By deault it will encrypt/decrypt 
+   text using 128-bit key size. 
+   """
+   parse = argparse.ArgumentParser(description=description)
 
    parse.add_argument("ifile", metavar="IN_FILE",type=str, help="The file to encrypt/decrypt")
    parse.add_argument("ofile", metavar="OUT_FILE", type=str, help="Output file")
@@ -63,31 +39,63 @@ def main():
    
    if args.loop == False:
       ## Gets the key ready
-      key = ""
-      for ch in args.key:
-         key += hex(ord(ch)).lstrip("0x")
-      
-      key = int("0x" + key.zfill(BLOCK_SIZE*2),16)
-      round_keys = key_expansion(key, NK)
+      round_keys = key_expansion(args.key, NK)
 
       if args.decrypt == True:
          ## Decrypt
-         for block in read_block(fd_in, BLOCK_SIZE):
-            cypher_text = int(block,16)
-            plain_text = str(hex(encrypt(cypher_text, round_keys, DECRYPT))).lstrip("0x").rstrip("L")
-            write_block(fd_out, str(plain_text))
-         fd_in.close()
-         fd_out.close()
+            plain_text = fd_in.read()
+            cypher_text = aes(plain_text, round_keys)
+            fd_out.write(bytearray(cypher_text))
+
+            fd_in.close()
+            fd_out.close()
       
       else: 
          ## Encrypt
-         for block in read_block(fd_in, BLOCK_SIZE):
-            plain_text = int(block,16)
-            cypher_text = str(hex(encrypt(plain_text, round_keys, ENCRYPT))).lstrip("0x").rstrip("L")
-            write_block(fd_out, str(cypher_text))
-            
+            cypher_text = fd_in.read()
+            plain_text = aes(cypher_text, round_keys, DECRYPT)
+            fd_out.write(bytearray(plain_text))
+
+            fd_in.close()
+            fd_out.close()
+   
+   if args.loop == True:
+
+         round_keys = key_expansion(args.key, NK)
+
+         ################################
+         # Encrypt
+         print "START ENCRYPTION"
+         plain_text = fd_in.read()
+         cypher_text = aes(plain_text, round_keys)
+         fd_out.write(bytearray(cypher_text))
+         
+                     
          fd_in.close()
          fd_out.close()
-   
+         
+         
+         ################################
+         # Decrypt
+         print "START DECRYPTION"
+         fd_in = open(args.ofile, "r")
+         fd_out = open("tmp.txt", "w")
+      
+         cypher_text = fd_in.read()
+         plain_text = aes(cypher_text, round_keys, DECRYPT)
+         fd_out.write(bytearray(plain_text))
+         
+         fd_in.close()
+         fd_out.close()
+         
+         with open(args.ifile, "r") as enc_file:
+            print "\n-------- Encrypted file ------\n{}".format(enc_file.read())
+
+         with open("tmp.txt", "r") as dec_file:
+            print "\n-------- Decryped file ------\n{}".format(dec_file.read())
+
+            
+
+
 if __name__ == "__main__":
    main()
